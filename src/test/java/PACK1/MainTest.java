f5260c20c58710f74d0ac9f067765983fef5662e
root <root@ip-172-31-38-50.ap-south-1.compute.internal>
2020-05-28 15:57:29 +0000

    first commit on maven-project

---
+++ src/test/java/PACK1/MainTest.java
@@ -0,0 +1,11 @@
+package gp1;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class MainTest {
+	@Test
+	public void simpleTest() {
+		assertThat(2 + 2).isEqualTo(4);
+	}
+}
